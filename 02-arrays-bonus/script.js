a59677dfae13bda4fccee222e38e8b643336392e
Leonardo-Corazzini <leocora10@gmail.com>
2024-10-16 16:24:47 +0200

    ADD: variante esercizio 1 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,9 +10,14 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+console.log(teachers)
+let nomeTolto 
+let nomeMesso
+for (let i = 0 ; i < teachers.length; i++){
+  nomeTolto = teachers.pop()
+  nomeMesso = teachers.splice(i,0,nomeTolto)
+}
  console.log(teachers)
-
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 let longNames = []
